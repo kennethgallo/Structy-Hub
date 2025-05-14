ddddc294054dd27ae7bae037353c83dd2deb2ca6
kennethgallo <61844301+kennethgallo@users.noreply.github.com>
2025-05-14 10:59:03 -0400

    Solved 'most frequent char' (python3)

---
+++ Hashing/014-most-frequent-char/most-frequent-char.py
@@ -0,0 +1,10 @@
+from collections import Counter
+
+def most_frequent_char(s):
+  count = Counter(s)
+
+  best = None
+  for char in s:
+    if best is None or count[char] > count[best]:
+      best = char
+  return best
