89c6bda93ac547f9f03c32741aa33c603014c5b5
Kenneth Gallo <61844301+kennethgallo@users.noreply.github.com>
2025-09-17 09:09:50 -0400

    Solved 'is prime' (python3)

--- Introduction/006-is-prime/is-prime.py
+++ Introduction/006-is-prime/is-prime.py
@@ -1,9 +1,11 @@
+from math import sqrt, floor
+
 def is_prime(n):
 
   if n < 2:
     return False
   
-  for i in range(2, n):
+  for i in range(2, floor(sqrt(n)) + 1):
     if n % i == 0:
       return False
 
