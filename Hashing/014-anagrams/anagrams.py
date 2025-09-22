4e6cc437c3878e3fa66d222a782596d83a20b36d
Kenneth Gallo <61844301+kennethgallo@users.noreply.github.com>
2025-09-22 13:33:16 -0400

    Solved 'anagrams' (python3)

---
+++ Hashing/014-anagrams/anagrams.py
@@ -0,0 +1,14 @@
+def anagrams(s1, s2):
+  return char_count(s1) == char_count(s2)
+
+def char_count(s):
+  count = {}
+
+  for char in s:
+    if char not in count:
+      count[char] = 0
+    count[char] += 1
+  
+  return count
+
+print(char_count('catss'))
