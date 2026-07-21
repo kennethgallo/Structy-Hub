e9c068490ba84a67f028137c9cb3507d8670fd59
Kenneth Gallo <61844301+kennethgallo@users.noreply.github.com>
2026-07-21 14:04:54 -0400

    Solved 'anagrams' (python3)

--- Hashing/017-anagrams/anagrams.py
+++ Hashing/017-anagrams/anagrams.py
@@ -1,13 +1,13 @@
 def anagrams(s1, s2):
   return counter(s1) == counter(s2)
 
+
 def counter(s):
-
-  count = {}
-
+  counter = {}
+  
   for char in s:
-    if char not in count:
-      count[char] = 0
-    count[char] += 1
+    if char not in counter:
+      counter[char] = 0
+    counter[char] += 1
+  return counter
 
-  return count
