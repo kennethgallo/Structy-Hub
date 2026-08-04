39a5f6d3027ac5fc9fa3365d9ca0b6442cf9f388
Kenneth Gallo <61844301+kennethgallo@users.noreply.github.com>
2026-08-04 14:00:31 -0400

    Solved 'pair product' (python3)

--- Hashing/020-pair-product/pair-product.py
+++ Hashing/020-pair-product/pair-product.py
@@ -5,5 +5,8 @@ def pair_product(numbers, target_product):
     complement = target_product / num
     if complement in previous_nums:
       return (previous_nums[complement], index)
+      # previous_nums[complement] <- this is taking the value, which is the           index of the value's location in numbers
 
-    
+    previous_nums[num] = index
+
+pair_product([3, 2, 5, 4, 1], 8) # -> (1, 3)
