6c7db83163a68f397e3cfd2a6f4fb1a38fee03fc
Kenneth Gallo <61844301+kennethgallo@users.noreply.github.com>
2026-08-26 09:22:56 -0400

    Solved 'exclusive items' (python3)

--- Hashing/023-exclusive-items/exclusive-items.py
+++ Hashing/023-exclusive-items/exclusive-items.py
@@ -1,28 +1,19 @@
 def exclusive_items(a, b):
   exclusives = []
   count = {}
-  # for num in a:
-  #   if num not in b:
-  #     exclusives.append(num)
-
-  # for ele in b:
-  #   if ele not in a:
-  #     exclusives.append(ele)
 
   for num in a:
-    if num not in count_a:
+    if num not in count:
       count[num] = 0
     count[num] += 1
 
   for num in b:
-    if num not in count_b:
+    if num not in count:
       count[num] = 0
     count[num] += 1 
 
   for num in count:
     if count[num] == 1:
       exclusives.append(num)
-
-  #print(count_a, count_b)
 
   return exclusives
