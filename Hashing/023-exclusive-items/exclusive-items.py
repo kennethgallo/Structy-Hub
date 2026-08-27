b579e8b5a5b8cbb1dd488b844497219a2c3eaf6a
Kenneth Gallo <61844301+kennethgallo@users.noreply.github.com>
2026-08-27 09:03:57 -0400

    Solved 'exclusive items' (python3)

--- Hashing/023-exclusive-items/exclusive-items.py
+++ Hashing/023-exclusive-items/exclusive-items.py
@@ -1,19 +1,16 @@
 def exclusive_items(a, b):
-  uniques = []
+  unqiues = []
 
   set_a = set(a)
   set_b = set(b)
 
-  for num in a:
-    if num not in set_b:
-      uniques.append(num)
+  for ele in set_a:
+    if ele not in set_b:
+      unqiues.append(ele)
 
-  for num in b:
+  for num in set_b:
     if num not in set_a:
-      uniques.append(num)
+      unqiues.append(num)
 
-  # time complexity -> O(n+m): time to convert lists to sets 
-  # and iterate through lists to add to uniques
-  # space complexity -> O(n+m): space for creating each set, combined
-
-  return uniques
+  print(unqiues)
+  
