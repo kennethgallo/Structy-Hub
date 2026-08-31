b3570eaff83cf9ba923c0ea7a19dacf0ac8a1aff
Kenneth Gallo <61844301+kennethgallo@users.noreply.github.com>
2026-08-31 10:14:39 -0400

    Solved 'anagrams' (python3)

--- Hashing/017-anagrams/anagrams.py
+++ Hashing/017-anagrams/anagrams.py
@@ -2,13 +2,11 @@ def anagrams(s1, s2):
   return count(s1) == count(s2)
 
 def counter(s):
-  count = {}
 
+  count = {}
+  
   for char in s:
     if char not in count:
       count[char] = 0
-
     count[char] += 1
   return count
-
-print(counter('fluster'))
