8a2163a5cd237522cce7d006dddac2f5eb6cc8b4
Kenneth Gallo <61844301+kennethgallo@users.noreply.github.com>
2026-09-01 08:50:40 -0400

    Solved 'sum numbers recursive' (python3)

--- Beginner-Recursion/032-sum-numbers-recursive/sum-numbers-recursive.py
+++ Beginner-Recursion/032-sum-numbers-recursive/sum-numbers-recursive.py
@@ -2,4 +2,4 @@ def sum_numbers_recursive(numbers):
   if len(numbers) == 0:
     return 0
 
-  return numbers[0] + sum_numbers_recursive(numbers)
+  return numbers[0] + sum_numbers_recursive(range(0, len(numbers)))
